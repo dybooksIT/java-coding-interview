8e3e2340b2226f6ebf1841667d633731e9ca46ac
Dongyang IT <dybooks.it@gmail.com>
2022-04-24 15:09:34 +0900

    edit ch09 files #9

--- 09/SwapTwoIntegers/src/main/java/coding/challenge/Bits.java
+++ 09/SwapTwoIntegers/src/main/java/coding/challenge/Bits.java
@@ -1,13 +1,11 @@
 package coding.challenge;
 
 public final class Bits {
-
     private Bits() {
         throw new AssertionError("Cannot be instantiated");
     }
         
     public static void swap(int p, int q) {
-        
         System.out.println();
         System.out.println("Before swap: p=" + p + ", q=" + q);
         p = p ^ q ^ (q = p);
--- 09/SwapTwoIntegers/src/main/java/coding/challenge/Main.java
+++ 09/SwapTwoIntegers/src/main/java/coding/challenge/Main.java
@@ -1,13 +1,10 @@
 package coding.challenge;
 
 public class Main {
-
     public static void main(String[] args) {
-
         Bits.swap(2, 4);
         Bits.swap(-2, 4);
         Bits.swap(0, 1);
         Bits.swap(42, 11);
     }
-
 }
