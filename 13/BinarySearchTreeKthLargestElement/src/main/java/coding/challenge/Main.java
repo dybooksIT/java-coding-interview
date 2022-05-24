cb855f1bc9a969174e41e925d22a0ba06d9e0ddb
Dongyang IT <dybooks.it@gmail.com>
2022-05-24 16:46:20 +0900

    edit ch13 files #7

--- 13/BinarySearchTreeKthLargestElement/src/main/java/coding/challenge/Main.java
+++ 13/BinarySearchTreeKthLargestElement/src/main/java/coding/challenge/Main.java
@@ -3,6 +3,14 @@ package coding.challenge;
 public class Main {
     public static void main(String[] args) {
         /*
+                  50
+                /    \
+              45      54
+                \    /  \
+                47  52  56
+                    /
+                   55
+
                   40
                 /    \
               30      45
@@ -13,6 +21,15 @@ public class Main {
         */
         BinarySearchTree<Integer> bt = new BinarySearchTree<>();
 
+        bt.insert(56);
+        bt.insert(55);
+        bt.insert(54);
+        bt.insert(52);
+        bt.insert(50);
+        bt.insert(47);
+        bt.insert(45);
+
+        /*
         bt.insert(40);
         bt.insert(30);
         bt.insert(45);
@@ -23,7 +40,7 @@ public class Main {
         bt.insert(5);
         bt.insert(17);
         bt.insert(41);
-        bt.insert(67);
+        bt.insert(67); */
 
         bt.kthLargest(3);
     }
