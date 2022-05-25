4a8b7c112e399ba6b81699ce644eba89376f4d25
Dongyang IT <dybooks.it@gmail.com>
2022-05-25 11:40:59 +0900

    edit ch13 files #8

--- 13/IsSymmetricBinaryTree/src/main/java/coding/challenge/Main.java
+++ 13/IsSymmetricBinaryTree/src/main/java/coding/challenge/Main.java
@@ -3,21 +3,21 @@ package coding.challenge;
 public class Main {
     public static void main(String[] args) {
         /*
-              40
+              50
             /    \
-          47      47
+          45      45
          /  \    /  \
-        11   3  3   11
+        41   3  3   41
         */
         BinaryTree<Integer> bt = new BinaryTree<>();
 
-        bt.insert(40);
-        bt.insert(47);
-        bt.insert(47);
-        bt.insert(11);
+        bt.insert(50);
+        bt.insert(45);
+        bt.insert(45);
+        bt.insert(41);
         bt.insert(3);
         bt.insert(3);
-        bt.insert(11);
+        bt.insert(41);
 
         System.out.println("Symmetry check recursive: " + bt.isSymmetricRecursive());
         System.out.println("Symmetry check iterative: " + bt.isSymmetricIterative());
