8439537cf5745ed9c38d88ba0eaad50e4532270d
Dongyang IT <dybooks.it@gmail.com>
2022-05-25 18:05:36 +0900

    edit ch14 files #3

--- 14/BucketSort/src/main/java/coding/challenge/Main.java
+++ 14/BucketSort/src/main/java/coding/challenge/Main.java
@@ -4,20 +4,25 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        // 배열 integers 초기화
-        int[] integers = {4, 1, 8, 3, 8, 2, 6, 7, 4, 9};
+        // 배열 integers1, integers2 초기화
+        int[] integers1 = {4, 2, 11, 7, 18, 3, 14, 7, 4, 16};
+        int[] integers2 = {4, 2, 8, 7, 8, 2, 2, 7, 4, 9};
 
         System.out.println("\n\nSort via Bucket sort:");
         System.out.println("-------------------------");
 
-        int[] integersC1 = integers.clone();
-        int[] integersC2 = integers.clone();
+        int[] integersC1 = integers1.clone();
+        int[] integersC2 = integers1.clone();
+        int[] integersC3 = integers2.clone();
+        int[] integersC4 = integers2.clone();
 
         BucketSort.sort1(integersC1);
         BucketSort.sort1(integersC2);
-        /* BucketSort.sort2(integersC1);
-        BucketSort.sort2(integersC2); */
+        // BucketSort.sort2(integersC3);
+        // BucketSort.sort2(integersC4);
         System.out.println("Sorted C1: " + Arrays.toString(integersC1));
         System.out.println("Sorted C2: " + Arrays.toString(integersC2));
+        // System.out.println("Sorted C3: " + Arrays.toString(integersC3));
+        // System.out.println("Sorted C4: " + Arrays.toString(integersC4));
     }
 }
--- 14/MergeSort/src/main/java/coding/challenge/Main.java
+++ 14/MergeSort/src/main/java/coding/challenge/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         // 배열 integers 초기화
-        int[] integers = {4, 5, 2, 7, 1};
+        int[] integers = {52, 28, 91, 19, 76, 33, 43, 57, 20};
 
         System.out.println("\n\nSort via Merge sort:");
         System.out.println("----------------------");
