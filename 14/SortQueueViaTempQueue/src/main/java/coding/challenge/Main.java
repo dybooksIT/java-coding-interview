b65f8f0518eda94c64946f8a43960635d2fdabd9
Dongyang IT <dybooks.it@gmail.com>
2022-05-26 11:58:12 +0900

    edit ch14 files #5

--- 14/SortQueueViaTempQueue/src/main/java/coding/challenge/Main.java
+++ 14/SortQueueViaTempQueue/src/main/java/coding/challenge/Main.java
@@ -6,6 +6,15 @@ import java.util.Queue;
 public class Main {
     public static void main(String[] args) {
         Queue<Integer> queue = new ArrayDeque<>();
+
+        queue.add(3);
+        queue.add(9);
+        queue.add(1);
+        queue.add(8);
+        queue.add(5);
+        queue.add(2);
+
+        /*
         queue.add(7);
         queue.add(1);
         queue.add(4);
@@ -15,7 +24,7 @@ public class Main {
         queue.add(-4);
         queue.add(1);
         queue.add(0);
-        queue.add(14);
+        queue.add(14); */
  
         System.out.println("Given queue: " + queue);
 
