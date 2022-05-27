087162fcd5813541976af1ef7b79f89515e2f209
Dongyang IT <dybooks.it@gmail.com>
2022-05-27 17:00:09 +0900

    edit ch14 files #7

--- 14/PositionOfFirstOne/src/main/java/coding/challenge/Main.java
+++ 14/PositionOfFirstOne/src/main/java/coding/challenge/Main.java
@@ -2,7 +2,7 @@ package coding.challenge;
 
 public class Main {
     public static void main(String[] args) {
-        int[] zerosAndOnes = {0, 0, 1, 1, 1, 1, 1};
+        int[] zerosAndOnes = {0, 0, 0, 1, 1, 1, 1};
 
         int index = Arrays.firstOneIndex(zerosAndOnes);
         System.out.println("Index: " + index);
