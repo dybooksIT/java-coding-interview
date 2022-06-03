ec0b90699de147bfa23a7a5309421ff67f6cff9a
Dongyang IT <dybooks.it@gmail.com>
2022-06-03 16:01:49 +0900

    edit ch17 files #3

--- 17/Factorial/src/main/java/coding/challenge/Main.java
+++ 17/Factorial/src/main/java/coding/challenge/Main.java
@@ -2,10 +2,10 @@ package coding.challenge;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("factorial1 function reault " + Factorial.factorial1(5));
-        System.out.println("factorial2 function reault " + Factorial.factorial2(5));
-        System.out.println("factorial3 function reault " + Factorial.factorial3(1, 5));
-        System.out.println("factorialTail function reault " + Factorial.factorialTail(5));
-        System.out.println("factorial4 function reault " +Factorial.factorial4(5));
+        System.out.println("factorial1 function result " + Factorial.factorial1(5));
+        System.out.println("factorial2 function result " + Factorial.factorial2(5));
+        System.out.println("factorial3 function result " + Factorial.factorial3(1, 5));
+        System.out.println("factorialTail function result " + Factorial.factorialTail(5));
+        System.out.println("factorial4 function result " +Factorial.factorial4(5));
     }
 }
