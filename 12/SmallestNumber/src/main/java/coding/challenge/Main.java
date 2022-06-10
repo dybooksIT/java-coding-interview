41fb3e874394690f929e80c0f5ae04c7ce2cd3ef
Dongyang IT <dybooks.it@gmail.com>
2022-06-10 10:46:07 +0900

    edit ch12 files #9

--- 12/SmallestNumber/src/main/java/coding/challenge/Main.java
+++ 12/SmallestNumber/src/main/java/coding/challenge/Main.java
@@ -2,12 +2,13 @@ package coding.challenge;
 
 public class Main {
     public static void main(String[] args) {
-        String nr = "4514327";
-        // String nr = "2222222";
+        String nr1 = "4514327";
+        String nr2 = "2222222";
 
-        Numbers.smallestAfterRemove(nr, 4);
+        Numbers.smallestAfterRemove(nr1, 4);
+        Numbers.smallestAfterRemove(nr2, 4);
 
-        /*
+        /* 원서 예제 코드
         String nr = "4321";
 
         Numbers.smallestAfterRemove(nr, 2); */
